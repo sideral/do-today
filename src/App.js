89122bcf33ef889c4018cc49ef30c230cd3649c0
sideral <a31415926535@gmail.com>
2019-03-31 22:17:11 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+import List from './List';
+import Form from './Form';
+
+function App() {
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('todo.items')) || []);
+  const [nextItemId, setNextItemId] = useState(0);
+
+  useEffect(function storeItems() {
+    localStorage.setItem('todo.items', JSON.stringify(items));
+    let next = items.reduce((id, item) => Number(item.id) > id? Number(item.id) : id, 0);
+    setNextItemId(next);
+  }, [items]);
+
+  const onItemAdded = function(text){
+    setItems(items.concat([{id: nextItemId + 1, text: text}]));
+    setNextItemId(nextItemId + 1);
+  };
+
+  const onItemDone = function(itemId){
+    setItems(items.filter((item) => item.id !== itemId));
+  };
+
+  return (
+    <div className="container todo-app">
+      <div className="row">
+        <div className="column">
+          <Form onItemAdded={onItemAdded}/>
+        </div>
       </div>
-    );
-  }
+      <div className="row">
+        <div className="column">
+          <List items={items} onItemDone={onItemDone}/>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
